d3481bd9df3cd039402275db9acad3ba35faad9d
emmanuelrapoo <aniemmanuelrapo@gmail.com>
2022-12-15 06:35:21 +0100

    showing blog to screen

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import IndexScreen from './src/screens/IndexScreen';
 import { Provider } from './src/context/BlogContext';
+import ShowScreen from './src/screens/ShowScreen';
 
 const Stack = createNativeStackNavigator();
 
@@ -12,6 +13,7 @@ function App() {
       <NavigationContainer>
         <Stack.Navigator initialRouteName='Index'>
           <Stack.Screen name="Index" component={IndexScreen} options={{ title: 'Index Page' }} />
+          <Stack.Screen name="Show" component={ShowScreen} options={{ title: 'Detail Page' }} />
         </Stack.Navigator>
       </NavigationContainer>
     </Provider>
--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,9 +1,10 @@
-import { FlatList, StyleSheet, Text, View, Button } from 'react-native'
+import { FlatList, StyleSheet, Text, View, Button, TouchableOpacity } from 'react-native'
 import React, { useContext } from 'react'
 import { Context } from '../context/BlogContext'
+import { Feather } from '@expo/vector-icons'
 
-const IndexScreen = () => {
-  const { state, addBlogPost } = useContext(Context)
+const IndexScreen = ({ navigation }) => {
+  const { state, addBlogPost, deleteBlogPost } = useContext(Context)
   return (
     <View>
       <Text>Index Screen</Text>
@@ -15,7 +16,18 @@ const IndexScreen = () => {
         data={state}
         keyExtractor={(blogPosts) => blogPosts.title }
         renderItem={({ item }) => {
-          return <Text>{item.title}</Text>
+          return(
+            <TouchableOpacity onPress={() => navigation.push('Show', { id: item.id })}>
+            <View style={styles.row}>
+              <Text style={styles.title}>{item.title} - {item.id}</Text>
+              <TouchableOpacity
+                onPress={() => deleteBlogPost(item.id)}
+              >
+                <Feather name='trash' style={styles.icon} />
+              </TouchableOpacity>
+            </View>
+            </TouchableOpacity>
+          )
         }}
       />
     </View>
@@ -24,4 +36,19 @@ const IndexScreen = () => {
 
 export default IndexScreen
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  row: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingVertical: 20,
+    paddingHorizontal: 10,
+    borderTopWidth: 1,
+    borderColor: 'gray'
+  },
+  title: {
+    fontSize: 18,
+  },
+  icon:{
+    fontSize: 24
+  }
+})
